c6de37e822e3a022b66b264d9da9ad5a7ded21f6
holma91 <alexanderholmberg91@gmail.com>
2024-12-04 14:48:55 +0100

    refactor old code up to the point that loading mnist works

--- ldm/data/base.py
+++ ldm/data/base.py
@@ -1,11 +1,14 @@
+import torch
+import torchvision
 from abc import abstractmethod
 from torch.utils.data import Dataset, ConcatDataset, ChainDataset, IterableDataset
 
 
 class Txt2ImgIterableBaseDataset(IterableDataset):
-    '''
+    """
     Define an interface to make the IterableDatasets for text2img data chainable
-    '''
+    """
+
     def __init__(self, num_records=0, valid_ids=None, size=256):
         super().__init__()
         self.num_records = num_records
@@ -13,7 +16,7 @@ class Txt2ImgIterableBaseDataset(IterableDataset):
         self.sample_ids = valid_ids
         self.size = size
 
-        print(f'{self.__class__.__name__} dataset contains {self.__len__()} examples.')
+        print(f"{self.__class__.__name__} dataset contains {self.__len__()} examples.")
 
     def __len__(self):
         return self.num_records
@@ -21,3 +24,27 @@ class Txt2ImgIterableBaseDataset(IterableDataset):
     @abstractmethod
     def __iter__(self):
         pass
+
+
+class MNISTWrapper(torch.utils.data.Dataset):
+    def __init__(self, root="data/mnist", train=True, download=True):
+        # Create MNIST dataset directly instead of receiving it
+        self.dataset = torchvision.datasets.MNIST(
+            root=root, train=train, download=download
+        )
+        self.transform = torchvision.transforms.Compose(
+            [
+                torchvision.transforms.ToTensor(),
+                torchvision.transforms.Normalize(
+                    mean=[-1.0], std=[2.0]
+                ),  # Scale from [0,1] to [-1,1]
+            ]
+        )
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, idx):
+        img, label = self.dataset[idx]
+        img = self.transform(img)
+        return {"image": img, "class_label": label}
